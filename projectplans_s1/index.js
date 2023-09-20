0f989660d639db3ca5b6c51163e5ce8141b42c7a
tiendung <tiendunga2k64@gmail.com>
2023-09-20 10:40:21 +0700

    hoan thanh tag + view

--- projectplans_s1/index.js
+++ projectplans_s1/index.js
@@ -1,5 +1,6 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "250px";
+  
 }
 
 // function closeNav()
@@ -9,6 +10,28 @@ function openNav() {
 
 let body = document.querySelector("body");
 
-body.addEventListener("dblclick",function(){
-    document.getElementById("mySidenav").style.width = "0px";
-})
+body.addEventListener("dblclick", function () {
+  document.getElementById("mySidenav").style.width = "0px";
+});
+
+function viewInfo() {
+  var block = document.getElementById("block");
+  var titP = document.querySelector(".info");
+  console.log(titP);
+  var paragraph = titP.textContent;
+
+  // Lấy thẻ <p> trong block và gán nội dung cho nó
+  var pElement = block.querySelector("p");
+  pElement.textContent = paragraph;
+
+  if (block.classList.contains("hidden")) {
+    block.classList.remove("hidden");
+  } else {
+    block.classList.add("hidden");
+  }
+}
+
+function closeBlock() {
+  var block = document.getElementById("block");
+  block.classList.add("hidden");
+}
